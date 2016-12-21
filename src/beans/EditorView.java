0ce9f583e627b1b01ab722a49ba9df19ba347782
ADESSO\mpouma <Thierry-Arnaud.Mpouma@adesso-insurance-solutions.de>
2016-12-21 22:48:12 +0100

    mes derniers changements (beans, notice, textediteur)...

---
+++ src/beans/EditorView.java
@@ -0,0 +1,17 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class EditorView {
+
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
